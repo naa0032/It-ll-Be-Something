f87fede08c9271137016db99dcbc107a39816ffa
Murad Hamirani <mh0107@mix.wvu.edu>
2020-05-07 16:29:25 -0400

    fixed bug in planting seeds

--- CS230/seedDetails.js
+++ CS230/seedDetails.js
@@ -3,9 +3,19 @@ function sunflowerDetails(){
   document.getElementById("sunflowerDetails").style.display = "block";
 }
 
+function plantSunflower(){
+  hideAllPlant();
+  document.getElementById("plantSunflower").style.display="block";
+}
+
 function roseDetails(){
   hideAll()
   document.getElementById("roseDetails").style.display = "block";
+}
+
+function plantRose(){
+  hideAllPlant();
+  document.getElementById("plantRose").style.display="block";
 }
 
 function tulipDetails(){
@@ -13,14 +23,29 @@ function tulipDetails(){
   document.getElementById("tulipDetails").style.display = "block";
 }
 
+function plantTulip(){
+  hideAllPlant();
+  document.getElementById("plantTulip").style.display="block";
+}
+
 function dahliaDetails(){
   hideAll()
   document.getElementById("dahliaDetails").style.display = "block";
 }
 
+function plantDahlia(){
+  hideAllPlant();
+  document.getElementById("plantDahlia").style.display="block";
+}
+
 function daffodilDetails(){
   hideAll()
   document.getElementById("daffodilDetails").style.display = "block";
+}
+
+function plantDaffodil(){
+  hideAllPlant();
+  document.getElementById("plantDaffodil").style.display="block";
 }
 
 function hideAll(){
@@ -29,4 +54,12 @@ function hideAll(){
   document.getElementById("tulipDetails").style.display="none";
   document.getElementById("dahliaDetails").style.display="none";
   document.getElementById("daffodilDetails").style.display="none";
+}
+
+function hideAllPlant(){
+  document.getElementById("plantSunflower").style.display="none";
+  document.getElementById("plantRose").style.display="none";
+  document.getElementById("plantTulip").style.display="none";
+  document.getElementById("plantDahlia").style.display="none";
+  document.getElementById("plantDaffodil").style.display="none";
 }
